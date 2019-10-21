689a2464cc2eed28738bc709c5f84a6d211a2bc3
miguelsalva <miguelsalva@gmail.com>
2019-10-21 21:21:40 +0200

    Change portscanner.c

--- portscanner.c
+++ portscanner.c
@@ -1,3 +1,10 @@
+/*
+ * FILE: portscanner.c
+ * AUTHOR: Miguel Salvá
+ * ABSTRACT: Simple TCP port scanner written in C to perform a benchmark
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <netdb.h>
@@ -5,34 +12,30 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define MAXPORT 1024
+#define MIN_PORT 0
+#define MAX_PORT 1023 
+/* System ports: 0-1023
+ * Registered ports: 1024-49151
+ * Private ports: 49152-65535 */
 
 
-main (int argc, char *argv[0]) {
-	int s, i;
-	struct sockaddr_in sa;
-	struct hostent *he;
+int main (int argc, char *argv[0]) {
+	int port;
+	int sockfd;
+	struct sockaddr_in address;
 
-	if ((he = gethostbyname (argv[1])) == NULL) {
-		perror ("gethostbyname");
-		exit(1); 
-	}
-	sa.sin_family = AF_INET;
-	sa.sin_addr = *((struct in_addr *)he-> h_addr);
+	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = inet_addr ((char *)argv[1]);
 
-	for (i = 1; i <= MAXPORT; i++) {
-		if ((s = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
-			perror ("socket");
-			exit (-1);
-		}
-		sa.sin_port = htons(i);
-
-		if (connect (s, (struct sockaddr *)&sa, sizeof (sa)) < 0)
-			close (s);
-		
-		else {
-			printf ("Port %d is open\n",i);
-			close (s);
+	for (port = MIN_PORT; port < MAX_PORT; port++) {
+		address.sin_port = htons(port);
+		if (connect (sockfd, (struct sockaddr *)&address, sizeof (address)) == 0) {
+			printf ("Port %d is open\n", port);
+			close (sockfd);
+			sockfd = socket (AF_INET, SOCK_STREAM, 0);
 		}
 	}
+	close (sockfd);
 }
+
