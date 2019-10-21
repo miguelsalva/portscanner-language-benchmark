030171edac2052d1a9e4267a02b0d2f65b476b8f
miguelsalva <miguelsalva@gmail.com>
2019-10-21 21:57:12 +0200

    Change portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
+#
+# FILE: portscanner.py
+# AUTHOR: Miguel Salvá
+# ABSTRACT: Simple TCP port scanner written in Python to perform a benchmark
+
 import sys
 import socket
 
 host = sys.argv[1]
-MAXPORT = 1024
+MIN_PORT= 0
+MAX_PORT = 1023
+# System ports: 0-1023
+# Registered ports: 1024-49151
+# Private ports: 49152-65535 
 
-s = socket.socket()
 
-for port in range(1,MAXPORT):
+for port in range(MIN_PORT,MAX_PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     if s.connect_ex((host,port)) == 0:
         print("Port " + str(port) + " is open")
-
-s.close()
+    s.close()
