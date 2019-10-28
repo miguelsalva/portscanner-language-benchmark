c85cb59916ceb7a06478b763269dc923ddb4bd7d
miguelsalva <miguelsalva@gmail.com>
2019-10-28 19:55:51 +0100

    Some minor changes in portscanner.c

--- portscanner.c
+++ portscanner.c
@@ -19,23 +19,23 @@
  * Private ports: 49152-65535 */
 
 
-int main (int argc, char *argv[0]) {
+int main(int argc, char *argv[0]) {
 	int port;
 	int sockfd;
 	struct sockaddr_in address;
 
-	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr ((char *)argv[1]);
+	address.sin_addr.s_addr = inet_addr((char *)argv[1]);
 
 	for (port = MIN_PORT; port < MAX_PORT; port++) {
 		address.sin_port = htons(port);
-		if (connect (sockfd, (struct sockaddr *)&address, sizeof (address)) == 0) {
-			printf ("Port %d is open\n", port);
-			close (sockfd);
-			sockfd = socket (AF_INET, SOCK_STREAM, 0);
+		if (connect(sockfd, (struct sockaddr *)&address, sizeof (address)) == 0) {
+			printf("Port %d is open\n", port);
+			close(sockfd);
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		}
 	}
-	close (sockfd);
+	close(sockfd);
 }
 
